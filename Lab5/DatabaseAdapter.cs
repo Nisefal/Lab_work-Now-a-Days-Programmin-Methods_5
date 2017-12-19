a80ce295c63681146d40c20387f887de0e0b24dd
Nisefal <kolyabogdanenko@gmail.com>
2017-12-20 00:22:54 +0200

    -
    -

---
+++ Lab5/DatabaseAdapter.cs
@@ -0,0 +1,72 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Lab5
+{
+    class DatabaseAdapter
+    {
+        static string connstring = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Lab5;Integrated Security=True";
+        public static DataSet GetClients()
+        {
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                SqlDataAdapter adapterClients = new SqlDataAdapter("SELECT * FROM Clients", conn);
+                DataSet dsCli = new DataSet();
+                adapterClients.Fill(dsCli);
+                return dsCli;
+            }
+        }
+
+        public static DataSet GetTransactionsClient(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                SqlDataAdapter adapterClients = new SqlDataAdapter("SELECT * FROM Transactions WHERE Id_client="+id.ToString(), conn);
+                DataSet dsCli = new DataSet();
+                adapterClients.Fill(dsCli);
+                return dsCli;
+            }
+        }
+        public static DataSet GetAllTransactions()
+        {
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                SqlDataAdapter adapterClients = new SqlDataAdapter("SELECT * FROM Transactions", conn);
+                DataSet dsCli = new DataSet();
+                adapterClients.Fill(dsCli);
+                return dsCli;
+            }
+        }
+
+        public static DataSet GetQuality()
+        {
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                SqlDataAdapter adapterClients = new SqlDataAdapter("SELECT * FROM Quality", conn);
+                DataSet dsCli = new DataSet();
+                adapterClients.Fill(dsCli);
+                return dsCli;
+            }
+        }
+
+        public static DataSet GetDisabledClients()
+        {
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                SqlDataAdapter adapterClients = new SqlDataAdapter("SELECT * FROM Disabeled", conn);
+                DataSet dsCli = new DataSet();
+                adapterClients.Fill(dsCli);
+                return dsCli;
+            }
+        }
+
+        public static void AddClient(string name, int money, string email)
+        {
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+
+                SqlDataAdapter adapterClients = new SqlDataAdapter("SELECT * FROM Disabled", conn);
+            }
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tClients of internet provider");
+            Output(DatabaseAdapter.GetClients());
+
+            Console.WriteLine("\t\tClient's transactions");
+            Output(DatabaseAdapter.GetTransactionsClient(1));
+
+            Console.WriteLine("\t\tAll transactions");
+            Output(DatabaseAdapter.GetAllTransactions());
+
+            Console.WriteLine("\t\tClients marks for quality");
+            Output(DatabaseAdapter.GetQuality());
+
+            Console.WriteLine("\t\tTemporary disabled clients");
+            Output(DatabaseAdapter.GetDisabledClients());
+
+            Console.ReadLine();
+
+
+        }
+
+        public static void Output(DataSet ds)
+        {
+            foreach (DataRow dataRow in ds.Tables[0].Rows)
+            {
+                foreach (var item in dataRow.ItemArray)
+                {
+                    Console.Write(item.ToString().Trim() + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
